ba59fcd4e675129aa25efe28fda260f75a46525e
Olewuezi Marvellous <marvyoha1@gmail.com>
2024-08-16 11:59:51 +0100

    Done with portfolio (For now)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1158281345"
+    serviceWorkerVersion: "192842878"
   }
 });
